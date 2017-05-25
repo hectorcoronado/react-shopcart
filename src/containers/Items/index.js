40de4edcaebc659ee92c3a374166ab8fbf725ded
hectorcoronado <coronado.hector@gmail.com>
2017-05-24 18:50:13 -0700

    wires up mapDispatchToProps in Items component

--- src/containers/Items/index.js
+++ src/containers/Items/index.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import './Items.css';
+// destructure ADD_TO_CART bc we're not export default-ing in actions
+import { addToCart } from '../../actions';
 
 class Items extends Component {
   renderList = () => {
     // 'items' comes from reduxStore, via mSTP func we created and passed to connect func below:
-    const { items } = this.props;
+    const { items, addToCartAction } = this.props;
 
     return items.map((item) => (
       <li
         key={item.id}
         className={'Items-list-item'}
-        onClick={() => console.log('added to store')}
+        onClick={() => addToCartAction(item)}
       >
         <img role="presentation" src={item.link} />
         <span>{item.title}</span>
@@ -32,6 +34,11 @@ class Items extends Component {
 // redux will send whatever piece of state we need, and make it available to our component via props:
 const mapStateToProps = (reduxState) => ({
   items: reduxState.items
-})
+});
 
-export default connect(mapStateToProps)(Items);
+// addToCartAction is a func that's invoked w/'item', which in turn invokes the dispatch func that is passed 'item'
+const mapDispatchToProps = (dispatch) => ({
+  addToCartAction: (item) => dispatch(addToCart(item))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Items);

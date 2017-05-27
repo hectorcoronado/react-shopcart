e1ce7e471f397b3707ed84d0bb97b0a99922899f
hectorcoronado <coronado.hector@gmail.com>
2017-05-26 18:01:47 -0700

    creates store with middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
 // create Redux Store, pass it all our reducers, & give redux devtools access:
-const store = createStore(
+const store = createStoreWithMiddleware(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__()
